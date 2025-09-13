89c817c8f483133203442f016b1702720e6ad01d
Tachasit <681102106@cpru.ac.th>
2025-09-13 16:16:20 +0700

    student profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
-import 'package:auto_checkin/firebase_options.dart'; // <<<--- 1. Import ไฟล์ที่ถูกสร้างขึ้นมา
-import 'package:auto_checkin/screens/login_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:auto_checkin/auth_gate.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart'; // <<<--- 2. Import ปลั๊กของเรา
+import 'firebase_options.dart';
 
-// 3. เปลี่ยน main ให้เป็น async และเพิ่มโค้ด Initialize
-Future<void> main() async {
-  // 4. ตรวจสอบให้แน่ใจว่า Widget ทั้งหมดพร้อมใช้งานก่อนเริ่มแอป
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // 5. "เปิดสวิตช์" Firebase โดยใช้ข้อมูลจาก firebase_options.dart
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
@@ -18,23 +14,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Check-in App', home: const AuthGate());
-  }
-}
-
-class AuthGate extends StatelessWidget {
-  const AuthGate({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return const LoginScreen();
-        }
-        return const Scaffold(body: Center(child: Text('You are logged in!')));
-      },
+    return MaterialApp(
+      title: 'Auto Check-in App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const AuthGate(),
     );
   }
 }
